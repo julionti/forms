239ffe30c94f831fdc3c4de1ff34ebeccaf1011e
HuLius <julionti@gmail.com>
2020-06-29 14:46:53 -0300

    Formulários reativos: Sincronizando HTML com FormGroup

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-data-form',
@@ -10,9 +10,14 @@ export class DataFormComponent implements OnInit {
 
   formulario: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+
+    this.formulario = this.formBuilder.group({ // assiciado a <form [formGroup]="formulario">
+      nome: null,
+      email: null
+    });
   }
 
 }
--- src/app/data-form/data-form.module.ts
+++ src/app/data-form/data-form.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { DataFormComponent } from './data-form.component';
+import { SharedModule } from './../shared/shared.module';
 
 
 @NgModule({
   declarations: [DataFormComponent],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SharedModule
   ]
 })
 export class DataFormModule { }
