acd26e8779115fdff9dd09aab4f88dfb06a1912f
HuLius <julionti@gmail.com>
2020-06-30 09:03:59 -0300

    Formulários reativos: Campos de endereço (migrando um form template driven para form reativo)

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -19,7 +19,14 @@ export class DataFormComponent implements OnInit {
 
     this.formulario = this.formBuilder.group({ // associado a <form [formGroup]="formulario">
       nome: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
-      email: [null, [Validators.required, Validators.email]]
+      email: [null, [Validators.required, Validators.email]],
+      cep: [null, Validators.required],
+      numero: [null, Validators.required],
+      complemento: [null],
+      rua: [null, Validators.required],
+      bairro: [null, Validators.required],
+      cidade: [null, Validators.required],
+      estado: [null, Validators.required],
     });
   }
 
